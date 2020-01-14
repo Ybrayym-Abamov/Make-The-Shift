bd78a7c80643b25523031dbe5c768ed274c52051
Mustafa Prodhan <mustafa.prodhan.317@gmail.com>
2020-01-14 12:28:28 -0500

    worked on footer and checked productDetail for review

--- src/react/components/ProductDetail.js
+++ src/react/components/ProductDetail.js
@@ -14,18 +14,18 @@ export function ProductDetail () {
         <div id="pictureBox">
          <Carousel afterChange={onChange}>
           <div className ="slide">
-          <img src="https://cdn.thingiverse.com/assets/bd/99/61/67/40/featured_preview_fussleiste_innere_ecke_90.stl"></img>
+          <img src="https://cdn.thingiverse.com/assets/bd/99/61/67/40/featured_preview_fussleiste_innere_ecke_90.stl" alt="preview_fussleite"></img>
           
         </div>
         <div className="slide">
-        <img src="https://assets.pinshape.com/uploads/image/file/189701/container_superstrings-puzzle-3d-printing-189701.jpg"></img>
+        <img src="https://assets.pinshape.com/uploads/image/file/189701/container_superstrings-puzzle-3d-printing-189701.jpg" alt="preview_puzzle"></img>
           
         </div>
         <div className="slide">
-          <img src="https://assets.pinshape.com/uploads/image/file/189702/container_superstrings-puzzle-3d-printing-189702.jpg"></img>
+          <img src="https://assets.pinshape.com/uploads/image/file/189702/container_superstrings-puzzle-3d-printing-189702.jpg" alt="preview_superstring"></img>
         </div>
         <div className ="slide">
-          <img src="https://assets.pinshape.com/uploads/image/file/2632/container_elephant-3d-printing-2632.jpg"></img>
+          <img src="https://assets.pinshape.com/uploads/image/file/2632/container_elephant-3d-printing-2632.jpg" alt="preview_elephant"></img>
         </div>
       </Carousel>
         </div>
